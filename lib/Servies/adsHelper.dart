234aeabfbfb38deddff1a92ed9dded613fce24d7
Abhijith JR <Achuabhijithjr@outlook.com>
2025-02-09 00:44:34 +0530

    App Icon again changed

--- lib/Servies/adsHelper.dart
+++ lib/Servies/adsHelper.dart
@@ -3,7 +3,8 @@ import 'dart:io';
 class AdHelper {
   static String get bannerAdUnitId {
     if (Platform.isAndroid) {
-      return 'ca-app-pub-3940256099942544/6300978111';
+      return 'ca-app-pub-8568607330093795/5126182677';
+      // return 'ca-app-pub-3940256099942544/6300978111';
     } else if (Platform.isIOS) {
       return '<YOUR_IOS_BANNER_AD_UNIT_ID>';
     } else {
